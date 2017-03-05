4714572ee910c5f6736cc58c3b414b786b6f89d7
Roman <a99dneyleta@Romans-MacBook-Air.local>
2017-03-05 16:42:02 +0200

    Initial Commit

---
+++ lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Roman on 3/5/17.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
